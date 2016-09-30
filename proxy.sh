0b7f4d59c74b63ae93be2e06c986ab092342148a
Rahul Kumar <rkhalliday1@gmail.com>
2016-09-30 18:26:53 +0000

    Updated on Sep 30 2016

--- proxy.sh
+++ proxy.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+# Last updated on Sep 30 2016
 
-#************ Property of Robotics Club IIT Delhi ****************
+#************ Property of Robotics Club IIT Delhi ****************#
 
-
-#***************************************************************************
+#***************************************************************************#
 # Set your boolean variables here
+
 Proxy=true
 GitHub=true
 SSH=true
 PPA=true
 Release=$(lsb_release -cs)
-Beagle=false
+Architecture=$(uname -m)
 
-#*********************************************************************************
+#*********************************************************************************#
 # Proxy settings
-if [ "$Proxy" = true ] ; then
+if [ "$Proxy" = true ]; then
 echo "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"
 http_proxy=http://10.10.78.22:3128
 https_proxy=https://10.10.78.22:3128
@@ -26,6 +27,9 @@ NO_PROXY=.iitd.ac.in,.iitd.ernet.in
 AUTO_PROXY=http://www.cc.iitd.ernet.in/cgi-bin/proxy.btech" > /etc/environment 
 
 
+if [ ! -f /etc/apt/apt.conf.d/99proxy ]; then
+touch /etc/apt/apt.conf.d/99proxy
+fi
 echo "Acquire::http::Proxy \"http://10.10.78.22:3128\";
 Acquire::https::Proxy \"https://10.10.78.22:3128\";
 Acquire::HTTP::PROXY \"http://10.10.78.22:3128\";
@@ -39,10 +43,14 @@ Acquire::HTTPS::PROXY \"https://10.10.78.22:3128\";" > /etc/apt/apt.conf
 
 echo "IIT Delhi Proxy Settings done !!"
 fi
-#**********************************************************************
+#**********************************************************************#
 
 if [ "$PPA" = true ] ; then
-# backup original sources.list file to sources.list.bak
+if [ "$Architecture" = "x86_64" ]; then
+if [ ! -f /etc/apt/sources.list.bak ];
+# backup the original sources.list file to sources.list.bak
+then cp /etc/apt/sources.list /etc/apt/sources.list.bak
+fi
 
 echo "# Binary packages IIT Delhi
 # ===============
@@ -62,9 +70,9 @@ deb http://repo.iitd.ernet.in/ubuntu $Release-security main restricted universe 
 
 echo "PPA settings done !!"
 fi
+fi
 
-
-#**********************************************************************
+#**********************************************************************#
 # GitHub settings
 if [ "$GitHub" = true ] ; then 
 echo "[url \"https://github.com/\"]
@@ -77,24 +85,31 @@ git config --global push.default simple
  
 echo "Github Settings done !!"
 fi
-#**********************************************************************
+
+#**********************************************************************#
 if [ "$SSH" = true ] ; then
+if [ "$Architecture" = "x86_64" ]; then
 echo "HOST beagle
     HostName 192.168.7.2
     User root" > ~/.ssh/config
 
 echo "SSH settings done !!"
 fi
-#**********************************************************************
+fi
+#**********************************************************************#
     
 ############### Some extra Stuff for Beagle #################
-# for kernel 3.8
-if [ "$Beagle" = true ] ; then
-echo "Setting Slots and pins path for beagle" 
+# for setting path variables for slots and pins.
+if [ "$Architecture" = "armv7l" ]; then 
+if [ "$Release" = "wheezy" ]; then 
 export SLOTS=/sys/devices/bone_capemgr.*/slots >> ~/.bashrc
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins >> ~/.bashrc
-source ~/.bashrc
+fi 
+
+if [ "$Release" = "jessie" ]; then
+export SLOTS=/sys/devices/platform/bone_capemgr/slots >> ~/.bashrc
+export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins >> ~/.bashrc
 fi
-
-
-
+source ~/.bashrc
+echo "Settings for Slots and Pins path done!!"
+fi
